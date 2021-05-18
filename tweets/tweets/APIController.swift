2f1c9551ca78220b7ac2a72ba14c9db6ac8874b3
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2021-05-18 12:40:01 +0300

    2

--- tweets/tweets/APIController.swift
+++ tweets/tweets/APIController.swift
@@ -11,5 +11,21 @@ import UIKit
 class APIController{
     
     let token: String
+    weak var delegat: APITwitterDelegate?
     
+    init(delegat: APITwitterDelegate?, token: String) {
+        
+        self.delegat = delegat
+        self.token = token
+    }
+    
+    func performRequest(string: String) {
+        
+        let req = string.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let url = URL(string: "https://api.twitter.com/1.1/search/tweets.json?q=\(req)&lang=fr&count=100")
+        
+        print(url!)
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "GET"
+    }
 }
