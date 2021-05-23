db71c7c9d387ae0807d4088a45e707453a7b129e
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2021-05-23 17:57:18 +0300

    ))

--- tweets/tweets/ViewController.swift
+++ tweets/tweets/ViewController.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, APITwitterDelegate, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var requestTextFiel: UITextField!
+    @IBOutlet weak var tweetText: UITextView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    let cellIdentifier = "cell"
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+           super.viewDidLoad()
+           
+           tokenRequest(callback: runApi)
+           self.requestTextField.delegate = self
+       }
 
 
 }
